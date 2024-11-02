86d52127f3d08c66b64092fa7f6b73c25473c9f5
Alan Cardozo <cardozoalani@hotmail.com>
2024-11-01 21:50:31 -0300

    feat(terraform): add DynamoDB tables for Accounts and Transactions

--- infra/terraform/main.tf
+++ infra/terraform/main.tf
@@ -45,17 +45,6 @@ resource "aws_sqs_queue" "transaction_queue" {
   name = "transaction-queue"
 }
 
-resource "aws_dynamodb_table" "transaction_table" {
-  name           = "TransactionTable"
-  billing_mode   = "PAY_PER_REQUEST"
-  hash_key       = "transactionId"
-
-  attribute {
-    name = "transactionId"
-    type = "S"
-  }
-}
-
 resource "aws_lambda_function" "transaction_processor" {
   function_name = "TransactionProcessor"
   filename      = "${path.module}/../lambda/lambda.zip"
@@ -78,4 +67,91 @@ resource "aws_lambda_event_source_mapping" "sqs_to_lambda" {
   event_source_arn = aws_sqs_queue.transaction_queue.arn
   function_name    = aws_lambda_function.transaction_processor.arn
   batch_size       = 10
+}
+
+resource "aws_dynamodb_table" "accounts_table" {
+  name           = "AccountsTable"
+  billing_mode   = "PAY_PER_REQUEST"
+  hash_key       = "accountId"
+
+  attribute {
+    name = "accountId"
+    type = "S"
+  }
+
+  tags = {
+    Name        = "AccountsTable"
+    Environment = "development"
+  }
+}
+
+resource "aws_dynamodb_table" "transaction_table" {
+  name           = "TransactionTable"
+  billing_mode   = "PAY_PER_REQUEST"
+  hash_key       = "transactionId"
+
+  attribute {
+    name = "transactionId"
+    type = "S"
+  }
+
+  attribute {
+    name = "accountId"
+    type = "S"
+  }
+
+  global_secondary_index {
+    name               = "AccountIdIndex"
+    hash_key           = "accountId"
+    projection_type    = "ALL"
+    read_capacity      = 5
+    write_capacity     = 5
+  }
+
+  tags = {
+    Name        = "TransactionsTable"
+    Environment = "development"
+  }
+}
+
+resource "aws_iam_role" "account_service_role" {
+  name = "account_service_execution_role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        }
+        Action = "sts:AssumeRole"
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy" "account_service_dynamodb_policy" {
+  name = "AccountServiceDynamoDBPolicy"
+  role = aws_iam_role.account_service_role.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect   = "Allow"
+        Action   = [
+          "dynamodb:PutItem",
+          "dynamodb:GetItem",
+          "dynamodb:UpdateItem",
+          "dynamodb:Scan"
+        ]
+        Resource = [
+          aws_dynamodb_table.accounts_table.arn,
+          aws_dynamodb_table.transaction_table.arn,
+          "${aws_dynamodb_table.transaction_table.arn}/index/AccountIdIndex"
+        ]
+      }
+    ]
+  })
 }
--- infra/terraform/outputs.tf
+++ infra/terraform/outputs.tf
@@ -6,6 +6,10 @@ output "dynamodb_table_name" {
   value = aws_dynamodb_table.transaction_table.name
 }
 
+output "dynamodb_table_name_accounts" {
+  value = aws_dynamodb_table.accounts_table.id
+}
+
 output "lambda_function_name" {
   value = aws_lambda_function.transaction_processor.function_name
 }
